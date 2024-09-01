e81f7a6c647ff35f7a0c8b6696af43b59e608421
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-01 01:07:39 +0100

    redacting logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -23,3 +24,37 @@ def filter_datum(fields: List[str], redaction: str,
     return re.sub(pattern,
                   lambda m: m.group(0).split('=')[0] + '=' + redaction,
                   message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Class constructor initializer,
+        accepts a list of strings
+
+        Args:
+            fields (List[str]): A list of strings
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record and redact sensitive fields.
+        """
+        # Get the original log message
+        original_message = record.getMessage()
+        # Obfuscate sensitive fields in the log message
+        filtered_message = filter_datum(self.fields, self.REDACTION,
+                                        original_message, self.SEPARATOR)
+        # Replace the original message in the log
+        # record with the filtered message
+        record.msg = filtered_message
+        # Use the parent class to format the log record
+        return super(RedactingFormatter, self).format(record)
