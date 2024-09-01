63e6f814dde8191303acf6aab8419522514e2e2f
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-01 02:54:31 +0100

    encrypting password, returning correct bytes

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Encrypting passwords"""
 import bcrypt
-from typing import ByteString
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """Hashes a password
 
     Args:
@@ -13,6 +12,7 @@ def hash_password(password: str) -> ByteString:
     Returns:
         ByteString: Returns a salted hashed password byte string
     """
-    password_to_bytes = password.encode()
-    hash_passwd = bcrypt.hashpw(password_to_bytes, bcrypt.gensalt())
+    password_to_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()  # automatically generate salt
+    hash_passwd = bcrypt.hashpw(password_to_bytes, salt)
     return hash_passwd
