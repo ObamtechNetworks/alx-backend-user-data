29ffed3b810dddca0133581bc0163e5d754014ec
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-03 23:31:31 +0100

    pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -48,7 +48,6 @@ class Auth:
         # Check if 'Authorization' header exists in the request headers
         return request.headers.get('Authorization', None)
 
-
     def current_user(self, request=None) -> TypeVar('User'):
         """returns the current user session
 
