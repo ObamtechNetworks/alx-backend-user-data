78468d839caf7a9e3e79134c8e0e55843e05e8b6
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-04 02:31:20 +0100

    get specific user via credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,11 @@
 """Setup the basic authentication classs
 inherits from Auth class
 """
-from api.v1.auth.auth import Auth
+from typing import TypeVar
 import base64
-
-"""Basic authentication module"""
+from api.v1.auth.auth import Auth
+from models.base import DATA
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -74,3 +75,30 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':')
         return email, password
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd:
+                                     str) -> TypeVar('User'):
+        """Returns the user instace based on his email and password
+
+        Args:
+            user_email (str): The user email address
+            user_pwd (str): the user password
+
+        Returns:
+            User (class): The User Object
+        """
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+
+        # check database file
+        if DATA is not None:
+            user_list = User.search({"email": user_email})
+            if user_list:
+                # Assuming emails are unique, get the first match
+                user = user_list[0]
+                # check if the password is correct
+                if user.is_valid_password(user_pwd):
+                    return user
+        # Return None if any check fails
+        return None
