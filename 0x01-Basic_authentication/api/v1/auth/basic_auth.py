0cd97d9e491d2435eb01da6d60e30e082c33e1c3
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-04 01:35:20 +0100

    base64 header check,method impelemntation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,22 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Class for implementing basic authentication"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Returns a base64 part of the Authorization header
+        for Basic Authentication
+
+        Args:
+            authorization_header (str): The authorization header
+
+        Returns:
+            str: The authorization header string
+        """
+        if not authorization_header:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        # Otherwise, return the value after Basic (after the space)
+        return authorization_header.split('Basic ')[1]
