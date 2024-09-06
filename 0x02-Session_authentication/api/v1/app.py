8f30e63a2fc8beefa2cd532ea360f9cd83fafe89
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-06 01:41:21 +0100

    session auth class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -25,6 +26,8 @@ if auth_env:
         auth = Auth()
     elif auth_env == 'basic_auth':
         auth = BasicAuth()
+    elif auth_env == 'session_auth':
+        auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Sessikon Authentication"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Base class for configuring session authentication"""
+    pass
