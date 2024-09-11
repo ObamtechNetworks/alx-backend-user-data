fd60f1645cf2357e9f5ea30f9dbd78c3af050028
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-11 02:46:04 +0100

    create a user and add to db

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """Adds user to the database
+
+        Args:
+            email (str): the user email
+            hashed_password: the hashed user password
+
+        Returns:
+            The created user
+        """
+        new_user = User()
+        # add given properties to user
+        new_user.email = email
+        new_user.hashed_password = hashed_password
+        self.save(new_user)
+        self.commit()
+        return new_user
+
+    # save user to database
+    def save(self, instance):
+        """saves instance to the session
+
+        Args:
+            instance (obj): instance to save
+        """
+        self._session.add(instance)
+
+    def commit(self):
+        """commit chanes to the database
+        """
+        self._session.commit()
