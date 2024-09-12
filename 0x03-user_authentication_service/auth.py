460f8976efc4ce2b2d057f62d7cb1e37efc452f2
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-12 21:12:52 +0100

    corrected indentation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,13 +5,13 @@ import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
-        """Hashes given password
+    """Hashes given password
 
-        Args:
-            password (str): given password to hash
+    Args:
+        password (str): given password to hash
 
-        Returns:
-            bytes: returns the password in bytes
-        """
-        password_to_bytes = password.encode("utf-8")
-        return bcrypt.hashpw(password_to_bytes, salt=bcrypt.gensalt())
+    Returns:
+        bytes: returns the password in bytes
+    """
+    password_to_bytes = password.encode("utf-8")
+    return bcrypt.hashpw(password_to_bytes, salt=bcrypt.gensalt())
