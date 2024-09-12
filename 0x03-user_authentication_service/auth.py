da641093919f768ed111abb3c769aac3f3cd1732
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-12 21:36:18 +0100

    implemet user registration model

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,49 @@
 import bcrypt
 
 
+from db import DB
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a user to the database
+
+        Args:
+            email (str): user email
+            password (str): user password
+
+        Returns:
+            User: return the registered user
+        """
+        # check if user with email exists
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # hash the passwrod
+            hashed_pwd = _hash_password(password)
+            # create the user with details
+            user = User(email=email, hashed_password=hashed_pwd)
+            # save user to the db
+            self._db.save(user)
+            # commit changes to the databases
+            self._db.commit()
+            # return user
+            return user
+        except InvalidRequestError:
+            raise ValueError("email and password is required")
+
+
 def _hash_password(password: str) -> bytes:
     """Hashes given password
 
