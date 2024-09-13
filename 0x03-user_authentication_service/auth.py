0e8a437396b4d335e5bc02919325a54704d1b7f9
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-09-13 14:56:54 +0100

    added docstring

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,6 +50,7 @@ class Auth:
             raise ValueError("email and password is required")
 
     def valid_login(self, email: str, password: str) -> bool:
+        """Validates user login"""
         try:
             # Find user by email
             user = self._db.find_user_by(email=email)
